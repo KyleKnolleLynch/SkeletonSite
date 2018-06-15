2b47c9b0512574e807158f07d48025ba73f73d23
Kyle Lynch <kylelynch104@gmail.com>
2018-06-14 17:48:56 -0700

    skeletonSite

---
+++ js/file-1.js
@@ -0,0 +1,7 @@
+function openSlideMenu() {
+    document.getElementById('slide-bar').style.width = "300px";
+}
+
+function closeSlideMenu() {
+    document.getElementById('slide-bar').style.width = "0px";
+}
